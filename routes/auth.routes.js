ca2844bb39a6dbeeb5d38e8fb54aab52ec579886
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2021-11-19 17:01:37 +0000

    add language array, create signup route and view

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,32 +1,45 @@
 const router = require("express").Router();
-const languages = require('languages');
+const languages = require("./../languages");
+const User = require("./../models/User.model");
+const fileUploader = require("../config/cloudinary.config");
 
 // AUTH ROUTES GO HERE:
 
 // GET  /signup
 router.get("/signup", (req, res) => {
-  res.render("auth/signup", {languages});
+  res.render("auth/signup", { languages });
 });
 
 // POST /signup
-router.post('/signup', (req, res) => {
+router.post("/signup", fileUploader.single("profilePictureURL"), async (req, res) => {
+  try {
+    const { username, password, languagesISpeak, languagesIWantToLearn, socialMediaLink } = req.body;
+    const fileOnCloudinary = req.file.path;
 
+    const createdUser = await User.create({
+      username,
+      password,
+      languagesISpeak,
+      languagesIWantToLearn,
+      socialMediaLink,
+      profilePictureURL: fileOnCloudinary,
+    });
+    res.redirect("/");
+    console.log(createdUser);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // GET /login
-router.get('/login', (req, res) => {
-    res.render("auth/login")
+router.get("/login", (req, res) => {
+  res.render("auth/login");
 });
 
 // POST /login
-router.post('/login', (req, res) => {
-
-});
+router.post("/login", (req, res) => {});
 
 // GET /logout
-router.get('/logout', (req, res) => {
-
-});
-
+router.get("/logout", (req, res) => {});
 
 module.exports = router;
