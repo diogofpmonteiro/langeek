2467c25960bd2782483c147c3f1204a0d9c6653b
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2021-11-19 18:03:13 +0000

    Finish login, signup routes

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 require("./config/config-index")(app);
 
 // default value for title local
-const projectName = "Project-LanGeek";
+const projectName = "project-langeek";
 const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 const languages = require("./../languages");
 const User = require("./../models/User.model");
 const fileUploader = require("../config/cloudinary.config");
+const bcrypt = require("bcryptjs");
+
+const SALT_ROUNDS = 10;
 
 // AUTH ROUTES GO HERE:
 
@@ -16,18 +19,46 @@ router.post("/signup", fileUploader.single("profilePictureURL"), async (req, res
     const { username, password, languagesISpeak, languagesIWantToLearn, socialMediaLink } = req.body;
     const fileOnCloudinary = req.file.path;
 
+    const usernameNotProvided = !username || username === "";
+    const passwordNotProvided = !password || password === "";
+
+    if (usernameNotProvided || passwordNotProvided) {
+      res.render("auth/signup-form", { errorMessage: "Provide username and password" });
+
+      return;
+    }
+
+    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+
+    if (!regex.test(password)) {
+      res.status(400).render("auth/signup-form", {
+        errorMessage:
+          "Password needs to have at least 8 chars and must contain at least one number, one lowercase and one uppercase letter.",
+      });
+      return;
+    }
+
+    const foundUser = await User.findOne({ username });
+    if (foundUser) {
+      throw new Error("The username already exists");
+    }
+
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const createdUser = await User.create({
       username,
-      password,
+      password: hashedPassword,
       languagesISpeak,
       languagesIWantToLearn,
       socialMediaLink,
       profilePictureURL: fileOnCloudinary,
     });
+
     res.redirect("/");
-    console.log(createdUser);
   } catch (error) {
     console.log(error);
+    res.render("auth/signup", { errorMessage: err.message || "Error while trying to sign up" });
   }
 });
 
@@ -37,7 +68,36 @@ router.get("/login", (req, res) => {
 });
 
 // POST /login
-router.post("/login", (req, res) => {});
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const usernameNotProvided = !username || username === "";
+    const passwordNotProvided = !password || password === "";
+
+    if (usernameNotProvided || passwordNotProvided) {
+      res.render("auth/login", { errorMessage: "Provide username and password" });
+
+      return;
+    }
+
+    const foundUser = await User.findOne({ username });
+    if (!foundUser) {
+      throw new Error("Wrong credentials");
+    }
+
+    const isCorrectPassword = await bcrypt.compare(password, foundUser.password);
+
+    if (!isCorrectPassword) {
+      throw new Error("Wrong credentials");
+    } else if (isCorrectPassword) {
+      req.session.user = foundUser;
+      res.redirect("/");
+    }
+  } catch (error) {
+    res.render("auth/login", { errorMessage: err.message || "Error while trying to login" });
+  }
+});
 
 // GET /logout
 router.get("/logout", (req, res) => {});
