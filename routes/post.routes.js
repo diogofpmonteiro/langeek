dc71515a709e482f7577a3a8df02d19e8e042d28
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2021-11-19 15:15:55 +0000

    add post.routes and user.routes comments

---
+++ routes/post.routes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+// GET /create-post
+
+// POST /create-post
+
+// GET /edit-post
+
+// POST /edit-post
+
+// POST /edit-post/delete
+
+module.exports = router;
---
+++ routes/user.routes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+// GET /:userId
+
+// GET /:userId/edit-user
+
+// POST /:userId/edit-user
+
+module.exports = router;
