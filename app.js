0ee57472b2014ed54b49e65880e32b00ef536697
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2021-11-19 15:39:37 +0000

    config file bug fix

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const hbs = require("hbs");
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
+require("./config/config-index")(app);
 
 // default value for title local
 const projectName = "Project-LanGeek";
