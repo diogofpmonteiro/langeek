0cf9eceaa05b8731c8bd221f94c138b9b990401b
Telma Rosário <telmarrosario@gmail.com>
2021-11-19 15:14:13 +0000

    add auth.routes template

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 
-/* GET home page */
+/* GET home page Also renders the search form */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
 // GET search=? query
-router.get("/search", (req, res) => {});
+router.get("/search", (req, res) => {
+
+  
+
+});
 
 module.exports = router;
