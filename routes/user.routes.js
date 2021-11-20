30631c4a5b8e6edb7e1918e28240d472c5b5b991
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2021-11-20 19:34:40 +0000

    update user.routes and user views

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("./../models/User.model");
 const Post = require("./../models/Post.model");
 const isLoggedIn = require("./../middlewares/is.logged.in");
+const languages = require("./../languages");
 
 // GET /:userId
 router.get("/:userId", isLoggedIn, async (req, res) => {
@@ -20,17 +21,22 @@ router.get("/:userId/edit-user", isLoggedIn, async (req, res) => {
   try {
     const userId = req.params.userId;
     const foundUser = await User.findById(userId);
-    res.render("profile/user-profile", { foundUser });
+    res.render("profile/edit-profile", { foundUser, languages });
   } catch (error) {
     console.log(error);
   }
 });
 
 // POST /:userId/edit-user
-router.post("/:userId/edit-user", (req, res) => {
+router.post("/:userId/edit-user", async (req, res) => {
   try {
-    const userId = req.params.userId;
     const { username, languagesISpeak, languagesIWantToLearn, socialMediaLink } = req.body;
+    const userInfo = { username, languagesISpeak, languagesIWantToLearn, socialMediaLink };
+    const userId = req.params.userId;
+
+    const updatedUser = await User.findByIdAndUpdate(userId, userInfo);
+
+    res.redirect("/");
   } catch (error) {
     console.log(error);
   }
