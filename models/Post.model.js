3eb18e694942941326879522d1d377cc2f67ae30
Telma Rosário <telmarrosario@gmail.com>
2021-11-21 16:21:55 +0000

    add timestamps in post model

--- models/Post.model.js
+++ models/Post.model.js
@@ -1,12 +1,18 @@
 const { Schema, model } = require("mongoose");
 
-const postSchema = new Schema({
+const postSchema = new Schema(
+  {
   title: { type: String, required: true },
   postPicture: { type: String, required: true },
   description: { type: String, required: true },
   languageTag: { type: String, required: true },
   author:{ type: Schema.Types.ObjectId, ref: "User" },
-});
+  },
+  {
+  // this second object adds extra properties: `createdAt` and `updatedAt`
+  timestamps: true,
+  }
+);
 
 // Post.findById(123).populate('author')
 
