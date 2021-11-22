4bfc45f0b60d68f28b538125a5c61bee66b1a5e4
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2021-11-22 14:20:37 +0000

    update readme models

--- models/Post.model.js
+++ models/Post.model.js
@@ -2,19 +2,16 @@ const { Schema, model } = require("mongoose");
 
 const postSchema = new Schema(
   {
-  title: { type: String, required: true },
-  postPicture: { type: String, required: true },
-  description: { type: String, required: true },
-  languageTag: { type: String, required: true },
-  author:{ type: Schema.Types.ObjectId, ref: "User" },
+    title: { type: String, required: true },
+    postPicture: { type: String, required: true },
+    description: { type: String, required: true },
+    languageTag: { type: String, required: true },
+    author: [{ type: Schema.Types.ObjectId, ref: "User" }],
   },
   {
-  // this second object adds extra properties: `createdAt` and `updatedAt`
-  timestamps: true,
+    timestamps: true,
   }
 );
-
-// Post.findById(123).populate('author')
 
 const Post = model("Post", postSchema);
 
