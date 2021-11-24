3010630291b42140e713478803a18e3c7cfe9a89
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2021-11-24 14:58:57 +0000

    commit conflict changes

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -4,24 +4,21 @@ const Post = require("./../models/Post.model");
 const Comment = require("./../models/Comment.model");
 const isLoggedIn = require("./../middlewares/is.logged.in");
 
+router.post("/add-comment/:postId", isLoggedIn, async (req, res) => {
+  try {
+    const postid = req.params.postId;
+    const { text } = req.body;
 
-router.post('/add-comment/:postId', isLoggedIn, async (req, res) => {
-    try {
-        const postid = req.params.postId;
-        const {text} = req.body;
-        
-       const createdComment = await Comment.create({
-        commentator: req.session.user._id,
-         text
-     });
+    const createdComment = await Comment.create({
+      commentator: req.session.user._id,
+      text,
+    });
 
-     await Post.findByIdAndUpdate(postid, {$push: {comments: createdComment._id}})
-     res.redirect('/');  
-
-    } catch (error) {
-        console.log(error);
-    }
-
+    await Post.findByIdAndUpdate(postid, { $push: { comments: createdComment._id } });
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
